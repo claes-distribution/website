d7dc5b040e42358cc8952354c1c91ae0e6b840c9
Jamie Vangeysel <jamie.vangeysel@simplintho.com>
2020-09-16 13:23:28 +0200

    removed old content,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,9 +23,7 @@ import { FooterComponent } from './footer/footer.component';
 import { CoreModule } from './core/core.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HttpClient } from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { NewsListComponent } from './news-list/news-list.component';
 import { NewsItemComponent } from './news-item/news-item.component';
@@ -33,36 +31,18 @@ import { ContactComponent } from './contact/contact.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { ContactOpeningComponent } from './contact-opening/contact-opening.component';
 import { ContactInfoComponent } from './contact-info/contact-info.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { AboutCompanyComponent } from './about-company/about-company.component';
-import { AboutHistoryComponent } from './about-history/about-history.component';
-import { AboutMissionComponent } from './about-mission/about-mission.component';
-import { AboutStrategyComponent } from './about-strategy/about-strategy.component';
-import { AboutOurAssetsComponent } from './about-our-assets/about-our-assets.component';
-import { OrderComponent } from './order/order.component';
-import { EmailDisclaimerComponent } from './email-disclaimer/email-disclaimer.component';
 import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HomeComponent,
-    AboutComponent,
-    AboutCompanyComponent,
-    AboutHistoryComponent,
-    AboutMissionComponent,
-    AboutStrategyComponent,
-    AboutOurAssetsComponent,
     NewsListComponent,
     NewsItemComponent,
     ContactComponent,
     ContactFormComponent,
     ContactOpeningComponent,
     ContactInfoComponent,
-    OrderComponent,
-    EmailDisclaimerComponent
   ],
   imports: [
     BrowserModule,
