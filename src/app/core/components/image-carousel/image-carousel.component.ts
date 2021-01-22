d1be0b8410f97ddad9733f730a3806ec41a2fea3
Jamie Vangeysel <jamie.vangeysel@simplintho.com>
2021-01-22 14:23:41 +0100

    changes for accesibility

--- src/app/core/components/image-carousel/image-carousel.component.ts
+++ src/app/core/components/image-carousel/image-carousel.component.ts
@@ -23,6 +23,10 @@ export class ImageCarouselComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     if (this.slides && this.slideCount > 0) {
+      this.slides.forEach(slide => {
+        slide.state = 'background'
+      })
+
       this.currentIndex = 0;
       if (this.slideCount > 1) {
         this.startRotation(true)
@@ -37,11 +41,18 @@ export class ImageCarouselComponent implements OnInit, OnDestroy {
   }
 
   startRotation(fixResize?: boolean) {
+    this.slides.forEach(slide => {
+      slide.state = 'background'
+    })
     // check timeout on current slide
-    const timeout = this.slides[this.currentIndex].duration | 4500
+    const currentSlide = this.slides[this.currentIndex]
+    const timeout = currentSlide.duration | 4500
+    currentSlide.state = 'fade-in'
+
     if (fixResize) {
       this.onResize()
     }
+    this.ref.markForCheck()
 
     this.timeout = window.setTimeout(() => {
       if (this.currentIndex < this.slideCount - 1) {
@@ -62,7 +73,16 @@ export class ImageCarouselComponent implements OnInit, OnDestroy {
     } else if (window.innerWidth >= 1140 && this.currentSize !== 'large') {
       this.currentSize = 'large'
     }
+    this.updateUrls();
     this.ref.markForCheck()
+  }
+
+  updateUrls() {
+    this.slides.forEach(slide => {
+      slide.css = this.sanitizer.bypassSecurityTrustStyle(
+        `url('${slide.url}${slide.url.indexOf('https://pcm.groupclaes.be/v3/content') === 0 ? '/' + this.culture : ''}?size=${this.currentSize}')`
+      );
+    })
   }
 
   selectIndex(index: number) {
@@ -80,27 +100,12 @@ export class ImageCarouselComponent implements OnInit, OnDestroy {
     // check if current slide has a href
     const href = this.slides[this.currentIndex].href
     if (href) {
-      window.open(href, '_blank');
+      window.open(href, '_blank')
     }
-  }
-
-  get css(): SafeStyle {
-    if (this.url.indexOf('https://pcm.groupclaes.be/v3/content') === 0) {
-      return this.sanitizer.bypassSecurityTrustStyle(
-        `url('${this.url}/${this.culture}?size=${this.currentSize}')`
-      )
-    }
-    return this.sanitizer.bypassSecurityTrustStyle(
-      `url('${this.url}?size=${this.currentSize}')`
-    )
   }
 
   get title(): string {
     return this.slides[this.currentIndex].title[this.culture]
-  }
-
-  private get url(): string {
-    return this.slides[this.currentIndex].url
   }
 
   get slideCount(): number {
@@ -117,4 +122,7 @@ export interface ImageSlide {
   title: CultureEntry
   href?: string | null
   duration?: number
+
+  state?: string
+  css?: SafeStyle
 }
