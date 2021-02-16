4227e64cb2227b14f6a2c891471f4ba6763588eb
Jamie Vangeysel <jamie.vangeysel@simplintho.com>
2021-02-16 15:27:22 +0100

    removed gitlab ci, added travis ci

--- karma.conf.js
+++ karma.conf.js
@@ -1,17 +1,16 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+const plugins = [
+  require('karma-jasmine'),
+  require('karma-chrome-launcher'),
+  require('karma-jasmine-html-reporter'),
+  require('karma-coverage-istanbul-reporter'),
+  require('@angular-devkit/build-angular/plugins/karma')
+]
 
-module.exports = function (config) {
+function config(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
+    plugins,
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
@@ -29,4 +28,6 @@ module.exports = function (config) {
     singleRun: false,
     restartOnFileChange: true
   });
-};
+}
+
+module.exports = config;
