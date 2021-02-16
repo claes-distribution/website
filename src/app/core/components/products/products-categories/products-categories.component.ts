d44f5960a4960e2bdc4e1d0190f2c3de469e0c0f
Jamie Vangeysel <jamie.vangeysel@simplintho.com>
2021-02-16 14:49:09 +0100

    implement caching for categories

--- src/app/core/components/products/products-categories/products-categories.component.ts
+++ src/app/core/components/products/products-categories/products-categories.component.ts
@@ -24,36 +24,71 @@ export class ProductsCategoriesComponent implements OnInit {
   ngOnInit(): void {
     if (this.parent != null) {
       this.showTitle = false;
-      this.categoriesService.getChildren(this.parent).subscribe((result: IGetCategoriesResult) => {
-        this.categories = result.categories.map(e => {
-          return {
-            id: e.id,
-            name: e.name,
-            description: e.description,
-            css: this.sanitizer.bypassSecurityTrustStyle(
-              `background-image:url('https://pcm.groupclaes.be/v3/miniature/dis/website/category-image/${e.id}');`
-            ),
-            href: `https://shop.claes-distribution.be/products/${e.name[this.culture]}/${e.id}`
-          }
-        })
-        this.ref.markForCheck()
-      })
+      this.loadChildCategories(this.parent)
     } else {
-      this.categoriesService.get().subscribe((result: IGetCategoriesResult) => {
-        this.categories = result.categories.map(e => {
-          return {
-            id: e.id,
-            name: e.name,
-            description: e.description,
-            css: this.sanitizer.bypassSecurityTrustStyle(
-              `background-image:url('https://pcm.groupclaes.be/v3/miniature/dis/website/category-image/${e.id}');`
-            ),
-            href: `/products/${e.name[this.culture]}?id=${e.id}`
-          }
-        })
-        this.ref.markForCheck()
-      })
+      this.loadCategories()
     }
+  }
+
+  private loadCategories() {
+    const cachedCategories = this.retrieveFromCache('be.claes-distribution.www.categories')
+
+    if (cachedCategories) {
+      this.categories = JSON.parse(cachedCategories).map(e => this.mapCategory(e))
+      this.ref.markForCheck()
+    }
+
+    this.categoriesService.get().subscribe((result: IGetCategoriesResult) => {
+      window.localStorage.setItem('be.claes-distribution.www.categories', JSON.stringify(result.categories))
+
+      this.categories = result.categories.map(e => this.mapCategory(e))
+      this.ref.markForCheck()
+    })
+  }
+
+  private loadChildCategories(parentId: number) {
+    const cachedChilCategories = this.retrieveFromCache('be.claes-distribution.www.categories-' + parentId)
+
+    if (cachedChilCategories) {
+      this.categories = JSON.parse(cachedChilCategories).map(e => this.mapChildCategory(e))
+      this.ref.markForCheck()
+    }
+
+    this.categoriesService.getChildren(parentId).subscribe((result: IGetCategoriesResult) => {
+      window.localStorage.setItem('be.claes-distribution.www.categories-' + parentId, JSON.stringify(result.categories))
+
+      this.categories = result.categories.map(e => this.mapChildCategory(e))
+      this.ref.markForCheck()
+    })
+  }
+
+  private mapChildCategory(e) {
+    return {
+      id: e.id,
+      name: e.name,
+      description: e.description,
+      css: this.sanitizer.bypassSecurityTrustStyle(
+        `background-image:url('https://pcm.groupclaes.be/v3/miniature/dis/website/category-image/${e.id}');`
+      ),
+      href: `https://shop.claes-distribution.be/products/${e.name[this.culture]}/${e.id}`
+    }
+  }
+
+  private mapCategory(e) {
+    return {
+      id: e.id,
+      name: e.name,
+      description: e.description,
+      css: this.sanitizer.bypassSecurityTrustStyle(
+        `background-image:url('https://pcm.groupclaes.be/v3/miniature/dis/website/category-image/${e.id}');`
+      ),
+      href: `/products/${e.name[this.culture]}?id=${e.id}`
+    }
+  }
+
+  private retrieveFromCache(cacheKey: string) {
+    const cachedItem = window.localStorage.getItem(cacheKey)
+    return cachedItem ?? null
   }
 
   get culture(): string {
