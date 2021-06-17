2d0cef215cf792db7189a031b1ba995718297477
Jamie Vangeysel <jamie.vangeysel@simplintho.com>
2021-06-17 09:11:59 +0200

    remove lowercase transform

--- src/app/core/components/products/products-categories/products-categories.component.ts
+++ src/app/core/components/products/products-categories/products-categories.component.ts
@@ -65,7 +65,6 @@ export class ProductsCategoriesComponent implements OnInit {
   }
 
   private mapChildCategory(e) {
-    console.log(e.name[this.culture].toLowerCase())
     return {
       id: e.id,
       name: e.name,
@@ -73,12 +72,11 @@ export class ProductsCategoriesComponent implements OnInit {
       css: this.sanitizer.bypassSecurityTrustStyle(
         `background-image:url('https://pcm.groupclaes.be/v3/miniature/dis/website/category-image/${e.id}');`
       ),
-      href: `https://shop.claes-distribution.be/products/${e.name[this.culture].toLowerCase()}/${e.id}`
+      href: `https://shop.claes-distribution.be/products/${e.name[this.culture]}/${e.id}`
     }
   }
 
   private mapCategory(e) {
-    console.log(e.name[this.culture].toLowerCase())
     return {
       id: e.id,
       name: e.name,
@@ -86,7 +84,7 @@ export class ProductsCategoriesComponent implements OnInit {
       css: this.sanitizer.bypassSecurityTrustStyle(
         `background-image:url('https://pcm.groupclaes.be/v3/miniature/dis/website/category-image/${e.id}');`
       ),
-      href: `/products/${e.name[this.culture].toLowerCase()}?id=${e.id}`
+      href: `/products/${e.name[this.culture]}?id=${e.id}`
     }
   }
 
