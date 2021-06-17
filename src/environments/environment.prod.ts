72f595c0d7e62ab7170f61921250d80263c61318
Jamie Vangeysel <jamie.vangeysel@simplintho.com>
2021-06-17 09:12:29 +0200

    update pcm api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   defaultLanguage: 'nl',
   api_url: 'https://api.groupclaes.be/',
-  pcm_url: 'https://pcm.groupclaes.be/v2/',
+  pcm_url: 'https://pcm.groupclaes.be/v3/',
   supportedLanguages: [
     'nl',
     'fr'
